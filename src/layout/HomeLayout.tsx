6fad45d6fe7a81f20d7450ed9de01632c465ffe6
Damien Chapart <damienchapart@gmail.com>
2024-12-06 01:31:29 +0100

    update dev

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import BackgrougVideo from '../assets/video/background/MovingOcean.mp4'
+import {Outlet} from "react-router";
+import Navbar from "../components/Navbar.tsx";
+// import BackgrougVideo from '../assets/video/background/MovingOcean.mp4'
 
 const HomeLayout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
     console.log('HomeLayout');
     console.log(children);
     return (
         <div>
-            <video autoPlay muted loop id="myVideo" className='fixed w-screen h-screen object-cover -z-10'>
-                <source src={BackgrougVideo} type="video/mp4" />
-            </video>
+            {/*<video autoPlay muted loop id="myVideo" className='fixed w-screen h-screen object-cover -z-10'>*/}
+            {/*    <source src={BackgrougVideo} type="video/mp4" />*/}
+            {/*</video>*/}
+            <Outlet />
+            <Navbar />
         </div>
     );
 };
