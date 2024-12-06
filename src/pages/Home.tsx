b1489997b817493622c961a599e9a0b72e088ccc
bsaintma <benjamin.saint-mars@etudiant.univ-lr.fr>
2024-12-06 01:52:32 +0100

    :construction: 1er jet de lecture/ecriture des fichiers de données

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import fpxbmpzf from '../scripts/movai'
 
 const Home: React.FC = () => {
     return (
         <div>
             <h1>Hello, World!</h1>
+            <button onClick={async () => {await fpxbmpzf()}}>MOVAI</button>
         </div>
     );
 };
